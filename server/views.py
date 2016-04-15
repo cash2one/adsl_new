66d4732feb5906144dcb9e0ec2704e08ee3e06fb
wgy-ts <wgy-ts@163.com>
2016-04-15 17:10:44 +0800

    coding

--- server/views.py
+++ server/views.py
@@ -7,7 +7,7 @@ from logging.handlers import TimedRotatingFileHandler
 import datetime, os, logging
 
 TM_DELTA = 60 * 60
-
+LB_IP = '183.61.70.113'
 
 # Create your views here.
 def getlogger(logfile='./log'):
@@ -65,17 +65,15 @@ def adsl_host_report(request):
             host = request.POST['host']
             ret = LineHosts.objects.filter(host=host)
             if len(ret) > 0:
-                # if adsl_ip != ret[0].adsl_ip:
                 ret[0].adsl_ip = adsl_ip
                 ret[0].last_update_time = datetime.datetime.now()
                 ret[0].status = 'available'
                 ret[0].save()
 
                 return HttpResponse('OK')
-                # else:
-                #     return HttpResponse(content='Need re-dail')
             else:
-                line = '100.100.100.100:8' + host.replace('seo', '')
+                n = '8' + host.replace('seo', '')
+                line = LB_IP + ':' + n
                 record = LineHosts(host=host, line=line, adsl_ip=adsl_ip, status='available')
                 record.save()
                 return HttpResponse('add new line, host:' + host + ' line:' + line)
